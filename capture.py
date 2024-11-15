88a714f98f4f2ff8aefdd80dfaa691c5ad23cdfa
viet.bm <buiminhviet93@gmail.com>
2024-11-15 08:57:29 +0700

    Implement for control arduino by serial port

--- capture.py
+++ capture.py
@@ -6,13 +6,20 @@ import torch
 import onnxruntime as rt
 from PIL import Image
 from torchvision import transforms
-
+import serial
 import hyperparams as hparams
 
 # note: download model and paste to code folder to work if want to update AI model
 MODEL_PATH = "lr_1e-3_bs_64_sche-f0.2-p6/gc_torchscript.onnx"
 class_names = ["cardboard_paper", "glass", "metal", "others", "plastic"]
 device = "cpu"
+
+# Serial communication setup
+# arduino_port = "COM3"  # Update with your port for window
+# for linux RPi5
+# arduino_port = "/dev/ttyUSB0"
+# baud_rate = 9600
+# ser = serial.Serial(arduino_port, baud_rate, timeout=1)
 
 # Set up the ONNX session with options
 sess_opt = rt.SessionOptions()
@@ -33,6 +40,29 @@ data_transforms = transforms.Compose([
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
+def send_to_arduino(class_label):
+    # Map class label to integer
+    class_to_int = {
+        "default": "0",
+        "cardboard_paper": "1",
+        "glass": "2",
+        "metal": "3",
+        "others": "4",
+        "plastic": "5"
+    }
+    class_int = class_to_int.get(class_label, "0")  # Default to "0" if not found
+
+    # Send the integer as a string to Arduino
+    ser.write(class_int.encode())
+    print(f"Sent {class_int} to Arduino for class '{class_label}'")
+
+
+def close_serial():
+    # Ensure the serial connection is closed when done
+    if ser.is_open:
+        ser.close()
+
+
 # Function to preprocess the image and run inference
 def classify_image(ort_session, img):
     # input_shape = ort_session.get_inputs()[0].shape
@@ -49,6 +79,7 @@ def classify_image(ort_session, img):
     outputs = torch.tensor(ort_outs[0])
     _, preds = torch.max(outputs, 1)
     return class_names[preds[0]]
+
 
 ### Camera
 # cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
@@ -74,7 +105,8 @@ while True:
 
     # draw a rectangle to crop the image
     feed_frame = frame.copy()
-    cv2.rectangle(feed_frame, ((FRAME_WIDTH-FRAME_HEIGHT)//2, 0), ((FRAME_WIDTH-FRAME_HEIGHT)//2 + FRAME_HEIGHT, FRAME_HEIGHT), (0, 255, 0), 1)
+    cv2.rectangle(feed_frame, ((FRAME_WIDTH - FRAME_HEIGHT) // 2, 0),
+                  ((FRAME_WIDTH - FRAME_HEIGHT) // 2 + FRAME_HEIGHT, FRAME_HEIGHT), (0, 255, 0), 1)
 
     # Display the frame
     cv2.imshow("Video Feed", feed_frame)
@@ -85,7 +117,8 @@ while True:
     # Capture the frame when spacebar is pressed
     if key == ord(' '):
         # crop the frame
-        frame = frame[:, (FRAME_WIDTH-FRAME_HEIGHT)//2:(FRAME_WIDTH-FRAME_HEIGHT)//2 + FRAME_HEIGHT]
+        frame = frame[:,
+                (FRAME_WIDTH - FRAME_HEIGHT) // 2:(FRAME_WIDTH - FRAME_HEIGHT) // 2 + FRAME_HEIGHT]
 
         # Save the captured frame as an image file
         captured_image_path = "capture.jpg"
@@ -95,6 +128,7 @@ while True:
         img = Image.open(captured_image_path)
         start_time = time.time()
         pred_class = classify_image(ort_session, img)
+        # send_to_arduino(pred_class)
         inference_time = time.time() - start_time
 
         # Display classification result
@@ -110,9 +144,6 @@ while True:
     elif key == ord('q'):
         break
 
-# release resources
 cap.release()
 cv2.destroyAllWindows()
-
-
-
+# close_serial()
